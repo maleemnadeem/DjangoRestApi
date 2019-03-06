a05eee484cf9e2b5477274de57cd36fa73b26a2b
maleemnadeem <aleemchougtai@gmail.com>
2019-03-06 21:48:35 +0500

    add methodes

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.views import Response
+from rest_framework import status
+
+from . import serializers
+
 # Create your views here.
+
+class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self,request,format=None):
+        an_apiview = [
+        'django framework',
+        'django rest_framework',
+        'Rest Api'
+        ]
+
+        return Response({'message':'hello','an_apiview':an_apiview})
+
+
+    def post(self,request):
+
+        serializer = serializers.HelloSerializer(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self,request,pk=None):
+        return Response({'method':'put'})
+
+    def patch(self,request,pk=None):
+        return Response({'method':'patch'})
+
+    def delete(self,request,pk=None):
+        return Response({'method':'delete'})
