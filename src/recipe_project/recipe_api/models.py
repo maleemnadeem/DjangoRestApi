4ea447d78694e261472fe623773a8faca58eda66
maleemnadeem <aleemchougtai@gmail.com>
2019-03-27 23:04:10 +0500

    working on recipe

--- src/recipe_project/recipe_api/models.py
+++ src/recipe_project/recipe_api/models.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-from django.db import models
 
 # Create your models here.
+
 class UserProfileManager(BaseUserManager):
     """Helps Django work with our custom user model."""
 
@@ -35,9 +36,10 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-class UserProfile(AbstractBaseUser,PermissionsMixin):
 
-    email = models.EmailField(max_length=255,unique=True)
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -45,16 +47,19 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS= ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
+        """Django uses this when it needs to get the user's full name."""
 
         return self.name
 
     def get_short_name(self):
+        """Django uses this when it needs to get the users abbreviated name."""
 
         return self.name
 
     def __str__(self):
+        """Django uses this when it needs to convert the object to text."""
 
         return self.email
--- src/recipe_project/recipe_api/views.py
+++ src/recipe_project/recipe_api/views.py
@@ -2,5 +2,35 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import status
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
+from . import serializers
+from . import models
+from . import permissions
+
 
 # Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+
+         return ObtainAuthToken().post(request)
