1f785aefbb44aba625d5841fdbc1c161f3294cac
maleemnadeem <aleemchougtai@gmail.com>
2019-04-01 23:03:05 +0500

    add follower

--- src/recipe_project/recipe_api/urls.py
+++ src/recipe_project/recipe_api/urls.py
@@ -9,6 +9,8 @@ router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
 router.register('login', views.LoginViewSet,base_name='login')
 router.register('addrecipe', views.AddNewRecipeViewSet,base_name='recipe')
+router.register('follow', views.FollowerViewSet,base_name='follower')
+
 
 urlpatterns = [
     url(r'', include(router.urls))
