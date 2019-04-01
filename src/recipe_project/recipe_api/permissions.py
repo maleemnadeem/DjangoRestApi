9f5501ce5e2135e678afd6d6dbec45c08d04a839
aleem <muhammad.aleem@arbisoft.com>
2019-04-01 17:00:45 +0500

    add new permission but not useable

--- src/recipe_project/recipe_api/permissions.py
+++ src/recipe_project/recipe_api/permissions.py
@@ -18,3 +18,17 @@ class UpdateOwnRecipe(permissions.BasePermission):
             return True
 
         return  obj.user_profile.id == request.user.id
+
+
+
+class ViewOwnRecipe(permissions.BasePermission):
+
+    def has_object_permission(self,request,view,obj):
+
+        if request.method in permissions.SAFE_METHODS:
+
+            return True
+
+        else:
+
+            return False
