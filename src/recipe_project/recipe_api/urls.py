56bc81bfe2729831d04d031500f6bb103de3dd9d
aleem <muhammad.aleem@arbisoft.com>
2019-04-03 19:08:30 +0500

    add follower emails

--- src/recipe_project/recipe_api/urls.py
+++ src/recipe_project/recipe_api/urls.py
@@ -10,6 +10,7 @@ router.register('profile', views.UserProfileViewSet)
 router.register('login', views.LoginViewSet,base_name='login')
 router.register('addrecipe', views.AddNewRecipeViewSet,base_name='recipe')
 router.register('follow', views.FollowerViewSet,base_name='follower')
+router.register('view_follwer_recipe', views.ViewFollowerRecipeViewSet,base_name='view_follwer_recipe')
 
 
 urlpatterns = [
