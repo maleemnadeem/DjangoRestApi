87c5f980a81cbfdddf8096497ac9826b2f4a3ba7
maleemnadeem <aleemchougtai@gmail.com>
2019-05-04 15:22:07 +0500

    pep8 applied

--- src/recipe_project/recipe_api/models.py
+++ src/recipe_project/recipe_api/models.py
@@ -5,47 +5,31 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
 
 class UserProfileManager(BaseUserManager):
-    """Helps Django work with our custom user model."""
-
     def create_user(self, email, name, password=None):
-        """Creates a new user profile."""
-
         if not email:
             raise ValueError('Users must have an email address.')
-
         email = self.normalize_email(email)
         user = self.model(email=email, name=name,)
-
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, email, name, password):
-        """Creates and saves a new superuser with given details."""
-
         user = self.create_user(email, name, password)
-
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
-
         return user
 
 
-
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserProfileManager()
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
@@ -61,12 +45,11 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         """Django uses this when it needs to convert the object to text."""
-
         return self.email
 
-class Recipe(models.Model):
 
-    user_profile = models.ForeignKey('UserProfile',on_delete=models.CASCADE)
+class Recipe(models.Model):
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
     title_text = models.CharField(max_length=255)
     description_text = models.TextField()
     direction_text = models.TextField()
@@ -77,8 +60,9 @@ class Recipe(models.Model):
 
         return self.title_text
 
+
 class Follower(models.Model):
-    user_profile = models.ForeignKey('UserProfile',on_delete=models.CASCADE)
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
     follower_email = models.EmailField(max_length=255)
 
     def __str__(self):
