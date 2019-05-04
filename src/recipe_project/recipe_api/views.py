25622785b0e1f59fbb713f70ca5105d717e9a640
maleemnadeem <aleemchougtai@gmail.com>
2019-05-04 15:22:43 +0500

    pep8 applied

--- src/recipe_project/recipe_api/views.py
+++ src/recipe_project/recipe_api/views.py
@@ -20,8 +20,6 @@ from . import permissions
 # Create your views here.
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-
-
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
@@ -29,55 +27,63 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 
 class LoginViewSet(viewsets.ViewSet):
-    # serializer_class =serializers.LoginSerializer
 
     def create(self, request):
-         auth_token = ObtainAuthToken().post(request).data
-         result = models.UserProfile.objects.filter(email=self.request.data.get('username',None))[0]
-         data =serializers.LoginSerializer(result).data
-         print("auth token is: ",auth_token)
-         new_dict = auth_token
-         new_dict.update(data)
-         # data['token'] = auth_token
-         return Response(new_dict)
+        serializer_class = serializers.LoginSerializer
+        print("create")
+        auth_token = ObtainAuthToken().post(request).data
+        result = models.UserProfile.objects.filter(
+        email=self.request.data.get('username', None))[0]
+        data = serializers.LoginSerializer(result).data
+        print("auth token is: ", auth_token)
+        new_dict = auth_token
+        new_dict.update(data)
+        # data['token'] = auth_token
+        return Response(new_dict)
+
 
 class AddNewRecipeViewSet(viewsets.ModelViewSet):
-    authentication_classes=(TokenAuthentication,)
+    authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.RecipeSerializer
     permission_classes = (permissions.UpdateOwnRecipe,)
 
-    def perform_create(self,serializer):
-        serializer.save(user_profile = self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
 
     def get_queryset(self):
-        recipes = models.Recipe.objects.all().filter(user_profile_id=self.request.user.id)
+        recipes = models.Recipe.objects.all().filter(
+        user_profile_id=self.request.user.id)
         return recipes
 
+
 class FollowerViewSet(viewsets.ModelViewSet):
-    authentication_classes=(TokenAuthentication,)
+    authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.FollowerSerializer
     permission_classes = (permissions.UpdateOwnFollower,)
 
-    def perform_create(self,serializer):
-        serializer.save(user_profile = self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
 
     def get_queryset(self):
-        follower = models.Follower.objects.all().filter(user_profile_id = self.request.user.id)
+        follower = models.Follower.objects.all().filter(
+        user_profile_id=self.request.user.id)
         return follower
 
+
 class ViewFollowerRecipeViewSet(viewsets.ModelViewSet):
-    authentication_classes=(TokenAuthentication,)
-    serializer_class=serializers.ViewFollowerRecipeSerializer
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ViewFollowerRecipeSerializer
 
     def get_queryset(self):
-        follower_recipe = models.Follower.objects.filter(user_profile_id = self.request.user.id).values_list('follower_email', flat=True)
-        #follower_recipe = models.Follower.objects.filter(user_profile_id = self.request.user.id)
+        follower_recipe = models.Follower.objects.filter(
+        user_profile_id=self.request.user.id).values_list(
+        'follower_email', flat=True)
         return follower_recipe
 
-    def list(self,request):
+    def list(self, request):
         queryset = self.get_queryset()
         followers_emails = list(queryset)
-        recipies = models.Recipe.objects.filter(user_profile__email__in=followers_emails)
-        #recipies = models.AddNewRecipe.objects.all()
-        serializer = serializers.RecipeSerializer(recipies,many=True)
+        recipies = models.Recipe.objects.filter(
+        user_profile__email__in=followers_emails)
+        serializer = serializers.RecipeSerializer(recipies, many=True)
         return Response(serializer.data)
