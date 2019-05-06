d2d700db4a8158138f5e9d64e64da46648a02131
maleemnadeem <aleemchougtai@gmail.com>
2019-05-06 22:18:40 +0500

    pep8 applied

--- src/recipe_project/recipe_api/permissions.py
+++ src/recipe_project/recipe_api/permissions.py
@@ -3,30 +3,23 @@ from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
+     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-
         return obj.id == request.user.id
 
 
 class UpdateOwnRecipe(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-
         if request.method in permissions.SAFE_METHODS:
-
             return True
-
         return obj.user_profile.id == request.user.id
 
 
 class UpdateOwnFollower(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-
         if request.method in permissions.SAFE_METHODS:
-
             return True
-
         return obj.user_profile.id == request.user.id
